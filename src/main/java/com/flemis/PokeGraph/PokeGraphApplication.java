7552da9f7e44d9ba837f5d44e9433e23996ea904
taldoflemis <coderflemis@gmail.com>
2022-12-21 09:46:34 -0300

    initial commit

---
+++ src/main/java/com/flemis/PokeGraph/PokeGraphApplication.java
@@ -0,0 +1,13 @@
+package com.flemis.PokeGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flemis/PokeGraph/PokeGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flemis.PokeGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
