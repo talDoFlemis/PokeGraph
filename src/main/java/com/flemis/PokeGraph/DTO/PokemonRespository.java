60ceabc29b159efc6771493e40d3146351c07f01
taldoflemis <coderflemis@gmail.com>
2022-12-21 16:54:12 -0300

    fix: not updating ranks

--- src/main/java/com/flemis/PokeGraph/DTO/PokemonRespository.java
+++ src/main/java/com/flemis/PokeGraph/DTO/PokemonRespository.java
@@ -3,31 +3,39 @@ package com.flemis.PokeGraph.DTO;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.transaction.Transactional;
 
 public interface PokemonRespository extends JpaRepository<Pokemon, Integer> {
 
     // Attack Queries
+    @Transactional
+    @Modifying
     @Query("UPDATE Pokemon p set p.attRank = p.attRank + 1 where p.id = :id")
-    public Pokemon incrementAttack(@Param("id") Integer id);
+    public void incrementAttack(@Param("id") Integer id);
 
     public List<Pokemon> findAllByOrderByAttRankDesc();
 
     public List<Pokemon> findTop25ByOrderByAttRankDesc();
 
     // Speed Queries
+    @Transactional
+    @Modifying
     @Query("UPDATE Pokemon p set p.spdRank = p.spdRank + 1 where p.id = :id")
-    public Pokemon incrementSpeed(@Param("id") Integer id);
+    public void incrementSpeed(@Param("id") Integer id);
 
     public List<Pokemon> findAllByOrderBySpdRankDesc();
 
     public List<Pokemon> findTop25ByOrderBySpdRankDesc();
 
     // Life Queries
+    @Transactional
+    @Modifying
     @Query("UPDATE Pokemon p set p.lifeRank = p.lifeRank + 1 where p.id = :id")
-    public Pokemon incrementLife(@Param("id") Integer id);
+    public void incrementLife(@Param("id") Integer id);
 
     public List<Pokemon> findAllByOrderByLifeRankDesc();
 
